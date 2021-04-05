aaf1a838cdee34047624a50daddb3ad7c1761c33
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2021-04-05 15:38:15 -0300

    Improved somethings

--- powershell/wsl_install.ps1
+++ powershell/wsl_install.ps1
@@ -6,14 +6,20 @@
         None;
 
     .Notes
-        Probabily will be needed to change some execution policies in the console where te script will be running, so i recommend using the following command on the terminal before executing the script
-            Set-ExecutionPolicy -Confirm -ExecutionPolicy Bypass -Force -Scope CurrentUser
+        This script run following the instructions given here: https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+    .Notes
+            Probabily will be needed to change some execution policies in the console where te script will be running,
+        so i recommend using the following command (line below w/o exclamation) on the terminal before executing the script
+        !   Set-ExecutionPolicy -Confirm -ExecutionPolicy Bypass -Force -Scope CurrentUser
 
 #>
 
 # Directory path where Batch is being executed;
 $dirPATH = Get-Location
-$logfile = "$dirPATH\log.txt"
+$logfile = "$dirPATH\log-wsl_install.txt"
+$url = "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi"
+$name = "$dirPATH\wsl_update_x64.msi"
 
 try {
     # Enabling the Windows Subsystem for Linux;
@@ -23,20 +29,22 @@ try {
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
     # Downloading the Linux kernel update package;
-    $WebClient = New-Object System.Net.WebClient
-    $WebClient.DownloadFile("https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi", "$dirPATH\wsl_update_x64.msi")
+    Invoke-RestMethod $url -Outfile $name
 
     # Executing the Linux kernel update package;
-    Start-Process -ArgumentList -NoProfile -File "$dirPATH\wsl_update_x64.msi" -Wait
+    Start-Process -FilePath $name -Wait
     # Deleting the Linux kernel update package;
-    Remove-Item -Path "$dirPATH\wsl_update_x64.msi"
+    Remove-Item -Path $name
 
     # Setting WSL 2 as your default version;
     wsl --set-default-version 2
 
+    Clear-host
     Write-Output "Made It"
     Restart-Computer -Confirm
 } catch {
-    Write-Output "Something goes wrong! See Log.txt"
+    Clear-host
+    Clear-Content -Force -LiteralPath $logfile
+    Write-Output "Something goes wrong! See log-wsl_install.txt"
     Out-File -Force -FilePath $logfile -InputObject $Error -Encoding utf8 -Width 128
 }
