a343ab4857b9bb1812acf721833012ba2f646c9b
Gabriel-Marino <gcmarino404@gmail.com>
2022-06-13 15:38:47 -0300

    update

--- shell/kali-set.sh
+++ shell/kali-set.sh
@@ -1,1 +1,9 @@
 sudo apt install -y kali-linux-everything
+
+sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+./install.sh
+if [-f "install.sh"]; then
+    rm install.sh
+fi
