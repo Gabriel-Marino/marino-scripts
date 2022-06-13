79a9ca4ab36aea6a0b69381df7598a88a7c31c72
Gabriel-Marino <gcmarino404@gmail.com>
2022-06-13 15:17:53 -0300

    test

---
+++ utils/twitter/src/tweet-utils.ts
@@ -0,0 +1,3 @@
+import { Client } from "twitter-api-sdk";
+
+const client = new Client("MY-BEARER-TOKEN");
---
+++ utils/youtube/youtube-utils.py
@@ -0,0 +1,4 @@
+from pytube import YouTube
+from youtubesearchpython import VideosSearch
+import ffmpeg
+import os
