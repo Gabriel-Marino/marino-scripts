62037b21859e12c1ec4f6a84375128dc611bb94e
Marino <56511879+Gabriel-Marino@users.noreply.github.com>
2022-06-27 18:11:50 -0300

    Update lib_wsl.sh

--- shell/lib_wsl.sh
+++ shell/lib_wsl.sh
@@ -11,6 +11,7 @@ source ~/.bashrc
 
 # ------------------------------------------------------------------- #
 sudo dpkg --configure -a
+sudo add-apt-
 
 sudo apt install wget gpg -y
 sudo apt install git openssh -y
@@ -19,22 +20,20 @@ sudo apt install git openssh -y
 #   Installing Clang compilator and some libraries;
 sudo apt install gcc -y
 sudo apt install g++ -y
-sudo apt install gnuplot -y
 sudo apt install libgsl-dev -y
 
 #   Installing Python enviroment and some libraries.
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-python3 -m pip install --upgrade pip
-pip3 install matplotlib
-pip3 install jupyterlab
-pip3 install notebook
-pip3 install seaborn
-pip3 install gifmaker
-pip3 install pandas
+# sudo apt install python3 -y
+# sudo apt install python3-pip -y
+# python3 -m pip install --upgrade pip
+# pip3 install matplotlib
+# pip3 install jupyterlab
+# pip3 install notebook
+# pip3 install seaborn
+# pip3 install gifmaker
+# pip3 install pandas
 
 # Node and Npm
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo apt install -y npm
 # sudo npm install -g n
@@ -42,58 +41,27 @@ sudo apt install -y npm
 # sudo npm install npm@latest -g
 # sudo npm install -g typescript react react-dom next --save-dev
 sudo npm install --global n npm@latest yarn helmet next react react-dom typescript @types/node @types/react
-
-# Deno and Yarn
-curl -fsSL https://deno.land/install.sh | sh
-npm install --global yarn
+sudo n latest
 
 # JavaDK
 sudo apt install java-common -y
-wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
-sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
-sudo apt install -y java-17-amazon-corretto-jdk
+sudo apt install -y default-jdk
+sudo apt install -y default-jre
+sudo apt install -y maven
 
-# MongoDB
-sudo apt install gnupg -y
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-sudo apt update
-sudo apt install -y mongocli
-sudo apt install --only-upgrade mongocli
-
-# asdf package manager
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
-echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
-echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
-asdf update
-
-# Julia, R
+# # asdf package manager
+# git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+# echo ". $HOME/.asdf/asdf.sh" >> ~/.bashrc
+# echo ". $HOME/.asdf/completions/asdf.bash" >> ~/.bashrc
+# asdf update
 
 # ruby
 sudo apt install ruby-full -y
+sudo gem install rails
 
 # # Haskell
 # curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-sudo apt install ghc
-
-# #SQL Lite
-# wget https://www.sqlite.org/2022/sqlite-autoconf-3380500.tar.gz
-# tar xvfx sqlite-autoconf-3380500
-# rm *.tar.gz
-# cd sqlite-autoconf-3380500
-# ./configure --prefix=/usr/local
-# make
-# make install
-# cd ../
-
-# # Rust
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-# # Lua
-# curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
-# tar zxf lua-5.4.4.tar.gz
-# cd lua-5.4.4
-# make all test
+sudo apt install -y ghc
 
 # ------------------------------------------------------------------- #
 sudo apt update -y && sudo apt upgrade -y
